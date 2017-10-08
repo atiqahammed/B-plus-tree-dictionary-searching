4099de36346310e0cae8d57dc5b6aa1cfa6c2941
Atiq Ahammed <30492609+atiqahammed@users.noreply.github.com>
2017-10-08 22:53:42 +0600

    Bangla string traversing

--- BanglaTest1.java
+++ BanglaTest1.java
@@ -5,7 +5,9 @@ public class BanglaTest1 {
 	public static void main(String[] args) {
 		String word = "বিড় দচড";
 		System.out.println(word);
-
+		
+		for(int i=0;i<word.length();i++)
+			   System.out.println(word.charAt(i));
 	}
 
 }
